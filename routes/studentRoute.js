2457df0e7081923c6101dd0f0cc2350b3e16fbee
Deepa <deepa@webdev2026>
2026-09-15 18:29:19 +0530

    Create Express RESTful API

---
+++ routes/studentRoute.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+router.get("/students", (req, res) => {
+    res.json({
+        message: "Students data"
+    });
+});
+
+router.post('/students',(req,res)=>{
+    res.json({
+        message:"students data is created"
+    })
+})
+
+router.put("/students/:id", (req, res) => {
+    res.json({
+        message: "Student updated successfully"
+    });
+});
+module.exports = router;
